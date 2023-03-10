eb500954daecfc0e43d372ad94d133e53b72dd1b
Pao Perez <pperez5@nait.ca>
2023-03-09 23:48:23 -0700

    Fix object model binding response error

---
+++ Controllers/CategoryWithProductsDTO.cs
@@ -0,0 +1,17 @@
+﻿using LudyCakeShop.Domain;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudyCakeShop.Controllers
+{
+    public class CategoryWithProductsDTO
+    {
+        public int _id { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public List<Product> products;
+    }
+}
